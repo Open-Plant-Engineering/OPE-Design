5402f3ba8ec895296457e5fee40fc79ad94d13b4
Shivang Kheradiya <shivangatul@gmail.com>
2025-11-05 16:50:35 +0100

    OCCT Design Added

---
+++ cMake/OCCT.cmake
@@ -0,0 +1,15 @@
+# Check if OCC_INSTALL_DIR is defined
+if(DEFINED OCC_INSTALL_DIR)
+    message(STATUS "OCC_INSTALL_DIR is defined as ${OCC_INSTALL_DIR}")
+
+    # Define other variables based on OCC_INSTALL_DIR
+    set(OCCT_INCLUDE_DIR "${OCC_INSTALL_DIR}/inc")
+    set(OCCT_LIB_DIR "${OCC_INSTALL_DIR}/win64/gcc/lib")
+    set(OCCT_BIN_DIR "${OCC_INSTALL_DIR}/win64/gcc/bin")
+
+    # Optionally, add these directories to include and link paths
+    include_directories(${OCCT_INCLUDE_DIR})
+    link_directories(${OCCT_LIB_DIR})
+else()
+    message(WARNING "OCC_INSTALL_DIR is not defined. OCCT paths will not be set.")
+endif()
